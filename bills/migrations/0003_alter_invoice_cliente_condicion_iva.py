03228bdca1ae807d0bfd40d41f899ee1049f66cc
melafrancom <melafrancom@gmail.com>
2026-05-09 11:21:48 -0300

    fix: resolver conflicto de migraciones bills - renombrar 0002 a 0003

--- bills/migrations/0003_alter_invoice_cliente_condicion_iva.py
+++ bills/migrations/0003_alter_invoice_cliente_condicion_iva.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('bills', '0001_initial'),
+        ('bills', '0002_initial'),
     ]
 
     operations = [
